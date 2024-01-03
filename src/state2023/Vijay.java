20d4b264163dd440d823aa5e7892f18f09ec3fe9
Simon Xu <simonxhy0404@gmail.com>
2024-01-02 19:20:33 -0600

    solved Vijay

--- src/state2023/Vijay.java
+++ src/state2023/Vijay.java
@@ -4,80 +4,126 @@ import java.util.*;
 import java.io.*;
 
 public class Vijay {
-
+    HashMap<Character, Node> hm;
+    int CDs;
+    int time;
+    int found;
     public void run() throws Exception {
-        Scanner f = new Scanner(new File("Data/state2023/Vijay.dat".toLowerCase()));
+        Scanner f = new Scanner(new File("Data/state2023/Vijay.dat"));
         //Scanner f = new Scanner(System.in);
 
-        int cases = f.nextInt();
-        while (cases-- > 0) {
-            int level = f.nextInt();
+        int t = f.nextInt();
+        while (t --> 0) {
+            int height = f.nextInt();
             int row = f.nextInt();
             int col = f.nextInt();
-            int o = f.nextInt();
-            int sec = f.nextInt();
-            int sl = 0;
-            int sr = 0;
-            int sc = 0;
-            char[][][] m = new char[level][row][col];
-            int[][][] shadow = new int[level][row][col];
-            for (int i = 0; i < level; i++) {
+            CDs = f.nextInt();
+            time = f.nextInt();
+            char[][][] mat = new char[height][row][col];
+            hm = new HashMap<>();
+            int CD = '1';
+            for (int i = 0; i < height; i++) {
                 for (int j = 0; j < row; j++) {
-                    String temp = f.next();
+                    mat[i][j] = f.next().toCharArray();
                     for (int k = 0; k < col; k++) {
-                        m[i][j][k] = temp.charAt(k);
-                        if (m[i][j][k] == 'S') {
-                            sl = i;
-                            sr = j;
-                            sc = k;
+                        if (mat[i][j][k] == 'O') {
+                            mat[i][j][k] = (char) CD++;
+                            hm.put(mat[i][j][k], new Node(mat[i][j][k], i, j, k));
+                        }
+                        else if (mat[i][j][k] == 'S' || mat[i][j][k] == 'E') {
+                            hm.put(mat[i][j][k], new Node(mat[i][j][k], i, j, k));
                         }
                     }
                 }
             }
-            int[] vl = {-1, 1, 0, 0, 0, 0};
-            int[] vr = {0, 0, -1, 1, 0, 0};
-            int[] vc = {0, 0, 0, 0, -1, 1};
-            Queue<Integer> q = new LinkedList<>();
-            q.add(sl);
-            q.add(sr);
-            q.add(sc);
-            q.add(0);
-            q.add(0);
-            while (!q.isEmpty()) {
-                int l = q.poll();
-                int r = q.poll();
-                int c = q.poll();
-                int cds = q.poll();
-                int s = q.poll();
-                if (l >= level || l < 0 || r >= row || r < 0 || c >= col || c < 0 || m[l][r][c] == '#' || s > sec || shadow[l][r][c] == 1) {
-                    continue;
+            for (char cur : hm.keySet()) {
+                boolean[][][] visited = new boolean[height][row][col];
+                int[] vh = {-1, 1, 0, 0, 0, 0};
+                int[] vr = {0, 0, -1, 1, 0, 0};
+                int[] vc = {0, 0, 0, 0, -1, 1};
+                Queue<Integer> q = new LinkedList<>();
+                q.add(hm.get(cur).sh);
+                q.add(hm.get(cur).sr);
+                q.add(hm.get(cur).sc);
+                q.add(0);
+                while (!q.isEmpty()) {
+                    int h = q.poll();
+                    int r = q.poll();
+                    int c = q.poll();
+                    int dis = q.poll();
+                    if (h < 0 || h >= height || r < 0 || r >= row || c < 0 || c >= col || mat[h][r][c] == '#' || visited[h][r][c]) {
+                        continue;
+                    }
+                    visited[h][r][c] = true;
+                    if (mat[h][r][c] != '.') {
+                        hm.get(cur).addEdge(hm.get(mat[h][r][c]), dis);
+                    }
+                    for (int i = 0; i < 6; i++) {
+                        q.add(h + vh[i]);
+                        q.add(r + vr[i]);
+                        q.add(c + vc[i]);
+                        q.add(dis + 1);
+                    }
                 }
-                if (cds == o && m[l][r][c] == 'E') {
-                    System.out.println(s);
-                }
-                if (m[l][r][c] == 'O') {
-                    cds++;
-                    shadow = new int[level][row][col];
-                }
-                for (int i = 0; i < 6; i++) {
-                    int ll = l + vl[i];
-                    int rr = r + vr[i];
-                    int cc = c + vc[i];
-                    q.add(ll);
-                    q.add(rr);
-                    q.add(cc);
-                    q.add(cds);
-                    q.add(s+1);
-                }
-                shadow[l][r][c] = 1;
             }
+            found = Integer.MAX_VALUE;
+            recur('S', 0, 0, new HashSet<>());
+            System.out.println(found == Integer.MAX_VALUE ? "None shall pass." : "# of steps achieved: " + found);
         }
 
         f.close();
     }
 
+    public void recur(char cur, int t, int o, HashSet<Character> visited) {
+        if (visited.contains(cur) || t > time) {
+            return;
+        }
+        if (cur == 'E' && o == CDs) {
+            found = Math.min(found, t);
+            return;
+        }
+        if (cur != 'S') {
+            o+=1;
+        }
+        visited.add(cur);
+        for (Edge e : hm.get(cur).edges) {
+            recur(e.des.name, t + e.dis, o, visited);
+        }
+        visited.remove(cur);
+    }
+
+    public class Node {
+        char name;
+        int sh;
+        int sr;
+        int sc;
+        ArrayList<Edge> edges;
+
+        public Node(char n, int h, int r, int c) {
+            name = n;
+            sh = h;
+            sr = r;
+            sc = c;
+            edges = new ArrayList<>();
+        }
+
+        public void addEdge(Node n, int d) {
+            Edge e = new Edge(n, d);
+            edges.add(e);
+        }
+    }
+
+    public class Edge {
+        Node des;
+        int dis;
+
+        public Edge(Node d, int di) {
+            des = d;
+            dis = di;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         new Vijay().run();
     }
-
 }
